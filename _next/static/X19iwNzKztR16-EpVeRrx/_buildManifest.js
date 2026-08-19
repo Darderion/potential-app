208e909cd56d7a6a6e1d3aef4be303ef1669e1d8
Darderion <Eobard1SA@gmail.com>
2026-08-19 04:14:13 +0300

    Updates

--- _next/static/X19iwNzKztR16-EpVeRrx/_buildManifest.js
+++ _next/static/X19iwNzKztR16-EpVeRrx/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/potential-app//_next/:path+",
+        "destination": "/potential-app/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
